df30f2f4ab607962ad4c9e991b6a7db430fedcb1
peter <ptmartinezsound@gmail.com>
2021-03-24 21:40:36 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,26 @@ var formSubmitHandler = function (event) {
 };
 
 
+var getCityWeather = function (user) {
+    var apiKey = "d97176eba089433e8724fc40b45c34ee";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                console.log(response);
+                response.json().then(function (data) {
+                    console.log(data);
+                    displayWeather(data, user);
+                });
+            } else {
+                alert("Error: " + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert("unable to retrieve weather data");
+        });
+};
+
 
 
 
